f755e8825a35cb5181ae064dbbee8e7ae0e387f1
Nathan Genetzky <nathan.genetzky@dojofive.com>
2021-01-13 23:35:26 -0600

    script: Add quick script to import bsp

---
+++ import_bsp.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+src="${1?}"
+dest='./bsp'
+
+if [ -d "${dest}" ]; then
+    rm -rf "${dest}"
+fi
+mkdir -p "${dest}"
+
+tar -x \
+    --strip-components=1 \
+    -C "${dest}" \
+    -f "${src}"
